bfb58a23efba2b1396fb200c4926d29ef2e7ab25
Dalcilene <dalcilene.ramos@gmail.com>
2024-05-19 18:55:55 -0300

    first commit

---
+++ src/compras/compras.js
@@ -0,0 +1,36 @@
+const calcularTotal = (ferramentas, comprar) => {
+    if (ferramentas.length === 0 || comprar.length === 0) {
+        throw new Error("Ambas as listas precisam ter ao menos um item.");
+    }  
+    
+    let total = 0;
+    let ferramentasEncontradas = [];
+    
+    for (let i = 0; i < comprar.length; i++) {
+        for (let j = 0; j < ferramentas.length; j++) {
+            if (ferramentas[j].nome === comprar[i]) {
+                ferramentasEncontradas.push(ferramentas[j]);
+                total += ferramentas[j].preco;                    
+            }        
+        }     
+    }
+    
+    if (ferramentasEncontradas.length === 0) {
+        throw new Error("Nenhuma ferramenta desejada encontrada.");
+    } 
+
+    let nomesFerramentas = '';
+    for (let i = 0; i < ferramentasEncontradas.length; i++) {
+        nomesFerramentas += ferramentasEncontradas[i].nome;
+        if (i < ferramentasEncontradas.length - 1) {
+            nomesFerramentas += ', ';
+        }
+    }
+
+    const resultado = `O valor a pagar pelas ferramentas (${nomesFerramentas}) é R$ ${total.toFixed(2)}`;
+    return resultado; 
+};
+
+module.exports = {
+    calcularTotal
+};
